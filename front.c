304ccc8226d261a7706caaac8a8828f23f6f4063
Brad <bachippi@svsu.edu>
2017-02-09 10:44:48 -0500

    Update front.c

--- front.c
+++ front.c
@@ -140,19 +140,19 @@ int lex() {
 			getChar();
 			while (charClass == LETTER || charClass == DIGIT) {
 				addChar();
-                if(lexeme[0] == "f" && lexeme[1] == "o" && lexeme[2] == "r"){
-                    nextToken = "for";
-                    getchar();
-                    break;
-                }else if(lexeme[0] == "i" && lexeme[1] == "f"){
-                    nextToken = "if";
-                    getchar();
-                    break;
-                }else if(lexeme[0] == "e" && lexeme[1] == "l" && lexeme[2] == "s" && lexeme[3] == "e"){
-                    nextToken = "else";
-                    getchar();
-                    break;
-                }
+				if(lexeme[0] == "f" && lexeme[1] == "o" && lexeme[2] == "r"){
+				    nextToken = FOR_CODE;
+				    getchar();
+				    break;
+				}else if(lexeme[0] == "i" && lexeme[1] == "f"){
+				    nextToken = IF_CODE;
+				    getchar();
+				    break;
+				}else if(lexeme[0] == "e" && lexeme[1] == "l" && lexeme[2] == "s" && lexeme[3] == "e"){
+				    nextToken = ELSE_CODE;
+				    getchar();
+				    break;
+				}
 				getChar();
 			}
 			nextToken = IDENT;
